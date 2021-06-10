7af377d92a752acdc57306d07b2787c520e48d12
OMNIAS <76260614+OMNIASProductManager@users.noreply.github.com>
2021-06-10 09:55:27 +0200

    Add files via upload

---
+++ Craf/Data/FacilityzContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Facilityz.Models;
+
+namespace Facilityz.Data
+{
+    public class FacilityzContext : DbContext
+    {
+        public FacilityzContext (DbContextOptions<FacilityzContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Facilityz.Models.Craf> Craf { get; set; }
+
+        public DbSet<Facilityz.Models.User> User { get; set; }
+    }
+}
---
+++ Craf/Data/FacilityzDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Facilityz.Models;
+
+namespace Facilityz.Data
+{
+    public class FacilityzDbContext : DbContext
+    {
+        public FacilityzDbContext (DbContextOptions<FacilityzDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Facilityz.Models.Craf> Craf { get; set; }
+    }
+}
